ba384d57371461451319d4da663044822c94ecae
tatabox2000 <tatabox2000@gmail.com>
2015-12-01 19:19:12 +0900

    findcontour

--- test.py
+++ test.py
@@ -14,8 +14,15 @@ im_temp = np.zeros_like((im.shape[0],im.shape[1]),np.uint8)
 im_temp2 = im_temp.copy()
 #im_sample = imLab2 < 100
 #print im_sample
-
+imgray = cv2.cvtColor(im,cv2.COLOR_RGBcontours,-1,(0,255,0),32GRAY)
 im_temp = imLab[:,:,0]
+
+ret,thresh = cv2.threshold(imgray,127,255,0)
+im2, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+cv2.drawContours(im,contours,-1,(0,0,255),1)
+cv2.imshow("",im)
+cv2.waitKey(0)
+print contours,hierarchy
 im_temp2 = imLab2[:,:,0]
 print im_temp
 #imgray=cv2.calcBackProjectvtColor(im,cv2.COLOR_BGR2RGB)
